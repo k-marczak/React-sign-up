b91ce55eca2aecd0a01f749600ac342f7c1ef53e
k-marczak <k_marczak@outlook.com>
2021-02-12 22:33:45 +0100

    set up front end, downloaded axios already

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import "bootstrap/dist/css/bootstrap.min.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      fullname:'',
+      username:'',
+      email:'',
+      password:''
+    }
+    this.changeFullName = this.changeFullName.bind(this)
+    this.changeEmail = this.changeEmail.bind(this)
+    this.changeUsername = this.changeUsername.bind(this)
+    this.changePassword = this.changePassword.bind(this)
+
+  }
+
+  changeFullName(event){
+    this.setState({
+      fullName:event.target.value
+    })
+  }
+
+  changeUsername(event){
+    this.setState({
+      username:event.target.value
+    })
+  }
+
+  changeEmail(event){
+    this.setState({
+      email:event.target.value
+    })
+  }
+
+  changePassword(event){
+    this.setState({
+      password:event.target.value
+    })
+  }
+
+
+
+  render(){
+    return (
+      <div>
+        <div className="container">
+          <div className="form-div">
+            <form>
+              <input type="text"
+              placeholder="Full Name"
+              onChange={this.changeFullName}
+              value={this.state.fullName}
+              className="form-control form-group" 
+              />
+
+              <input type="text"
+              placeholder="Username"
+              onChange={this.changeUsername}
+              value={this.state.Username}
+              className="form-control form-group" 
+              />
+
+              <input type="text"
+              placeholder="E-mail"
+              onChange={this.changeEmail}
+              value={this.state.email}
+              className="form-control form-group" 
+              />
+
+              <input type="password"
+              placeholder="Password"
+              onChange={this.changePassword}
+              value={this.state.password}
+              className="form-control form-group" 
+              />
+
+              <input type="submit" className="btn btn-danger btn-block" value="Submit" />
+
+
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
