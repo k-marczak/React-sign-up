e4fd453e5d0b9dfff7191ac2d054a53fe6a8ee3c
k-marczak <k_marczak@outlook.com>
2021-02-12 22:54:07 +0100

    finish the application

--- signupbackend/routes/routes.js
+++ signupbackend/routes/routes.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const router = express.Router();
 const signUpTemplateCopy = require('../models/SignUpModels');
+const bcrypt = require('bcrypt')
 
 
-router.post('/signup', (request, response) => {
+router.post('/signup', async (request, response) => {
+
+    const saltPassword = await bcrypt.genSalt(10)
+    const securePassword = await bcrypt.hash(request.body.password, saltPassword)
+
+
     const signedUpUser = new signUpTemplateCopy({
         fullName:request.body.fullName,
         username:request.body.username,
         email:request.body.email,
-        password:request.body.password
+        password:securePassword
     })
 
     signedUpUser.save()
